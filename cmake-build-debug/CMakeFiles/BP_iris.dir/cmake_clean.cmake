24d10bcfacd975854fb4a0f9ca80163ddc9fa6e2
Qi <56908202+qi-xmu@users.noreply.github.com>
2021-05-18 10:16:49 +0800

    1

---
+++ cmake-build-debug/CMakeFiles/BP_iris.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BP_iris.exe"
+  "BP_iris.exe.manifest"
+  "BP_iris.pdb"
+  "CMakeFiles/BP_iris.dir/main.cpp.obj"
+  "libBP_iris.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BP_iris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
