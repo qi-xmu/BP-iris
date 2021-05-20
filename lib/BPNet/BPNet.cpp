066098f304881661614ebd671d1ffdb9b4a5d32e
Qi <56908202+qi-xmu@users.noreply.github.com>
2021-05-20 23:13:34 +0800

    完成基本网络层

--- lib/BPNet/BPNet.cpp
+++ lib/BPNet/BPNet.cpp
@@ -3,3 +3,72 @@
 //
 
 #include "BPNet.h"
+
+#include <ctime>
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/* 构造函数 */
+Layer::Layer() {};
+
+Layer::Layer(int node_num, vector<double> value) {
+    this->node_num = node_num;
+    this->node_value = value;
+}
+
+/* 输入函数：节点数，节点值 */
+void Layer::input(int dim, v_double &value) {
+    this->node_num = dim;
+    this->node_value = value;
+}
+
+/* 输出函数 */
+v_double Layer::output() {
+    return node_value;
+}
+
+/* 隐藏层构造函数 */
+hiddenLayer::hiddenLayer(int pre_node_num, int node_num) {
+    /* 要多一个，保证有偏置值。 */
+    this->pre_node_num = pre_node_num;
+    this->node_num = node_num;
+    initWeight();
+}
+
+/* 初始化神经网络层权重 */
+void hiddenLayer::initWeight() {
+    /* i个节点，每个节点有j个权重 */
+    for (int i = 0; i < node_num; i++) {
+        v_double w;
+        w.reserve(pre_node_num);
+        for (int j = 0; j < pre_node_num; j++) {
+            /*  w.i.j  -100.00 ~ 100.00 */
+            w.push_back((rand() % 10000) / 100.0);
+        }
+        W.push_back(w);
+    }
+}
+
+/* 计算节点输出值 */
+v_double hiddenLayer::getNodeValue(v_double &value) {
+    /* 计算一个节点的权重 */
+    for (int i = 0; i < node_num; i++) {
+        for (int j = 0; j < node_num; j++) {
+            node_value[i] += W[i][j] * value[j];
+        }
+        sigmod(node_value[i]);
+    }
+    return node_value;
+}
+
+/* 激活函数 */
+void hiddenLayer::sigmod(double &x) {
+    x = 1.0 / (1.0 + exp(-x));
+}
+
+
+
+
--- lib/BPNet/BPNet.h
+++ lib/BPNet/BPNet.h
@@ -5,11 +5,72 @@
 #ifndef BP_IRIS_BPNET_H
 #define BP_IRIS_BPNET_H
 
+#include <vector>
+
+#define v_double vector<double>
+
+using namespace std;
+
+/* 神经元层类 神经网络层设计
+ * * 基类 Layer 属性
+ * 值：节点数量，各个节点权重
+ * 行为：构造，输入，输出
+ *
+ * * 派生类 inputLayer 属性
+ * 行为：输入
+ *
+ * * 派生类 hiddenLayer 属性
+ * 值：前网络层节点个数
+ * 行为：计算，更新，激活函数
+ *
+ * * 派生类 outputLayer 属性
+ * 行为：计算残差
+ *
+ * 将 inputLayer 合并到 Layer 中
+ * 将 outputLayer 合并到 hiddenLayer 中
+ * */
+
+class Layer {
+public:
+    Layer();
+
+    Layer(int node_num, v_double value);
+
+    /* 输入函数 */
+    void input(int dim, v_double &value);
+    /* 输出函数 */
+    v_double output();
+
+protected:
+    vector<v_double > W;    /* 权重 */
+    vector<v_double > R;    /* 残差 */
+    v_double node_value;    /* 节点输出值 */
+    int node_num;           /* 网络层节点个数 */
+};
+
+/* 隐藏神经元层 */
+class hiddenLayer : public Layer {
+public:
+    hiddenLayer(int pre_node_num, int node_num);
+
+    /* 初始化权值 */
+    void initWeight();
+    /* 计算节点值并返回 */
+    v_double getNodeValue(v_double &value);
+
+private:
+    int pre_node_num;           /* 前节点个数，用于标记权重的维度 */
+    void sigmod(double &x);     /* 激活函数 */
+};
+
 
 class BPNet {
 public:
 
-
+private:
+    Layer input_layer;                  /* 输入层 */
+    vector<hiddenLayer> hidden_layers;  /* 隐藏层 */
+    hiddenLayer output_layer;           /* 输出层 */
 };
 
 
