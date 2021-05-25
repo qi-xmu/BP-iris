53e47a57d326dca923a77a1f061a652bf418b09e
Qi <56908202+qi-xmu@users.noreply.github.com>
2021-05-25 10:53:22 +0800

    得到完全100%模型
    精度问题导致准确度下降

--- include/BPNet.h
+++ include/BPNet.h
@@ -39,14 +39,17 @@ public:
 
     /* 设置初始值 */
     void set(int dim);
+
     /* 输入函数 */
     void input(v_double &value);
     /* 输出函数 */
     v_double output();
+
     /* 返回权重节点个数 不包括偏置节点 */
     int nodeSize() const { return node_num; };
+
     /* 保存使用:到处网络权重值 */
-    vector<v_double > nodeWeights() const{ return W; };
+    vector<v_double > nodeWeights() const { return W; };
 
 protected:
     int node_num;                       /* 网络层权重节点个数，不包含偏置节点 */
@@ -59,19 +62,23 @@ public:
 
     /* 设置网络层参数 */
     void set(int pre_node_num, int node_num);
+
     /* 初始化权值 */
     void initWeight();
     /* 计算节点输出值并返回 */
     v_double calNodeValue(vector<double> value);
+
     /* 计算各个节点的残差 */
     void calNodeResidual(v_double value);
     /* 返回节点传递计算值 */
     v_double nodeBackValue();
+
     /* 更新权值和偏置 */
     void updateWeights(v_double pre_node_value,
                        double learning_rate);
     /* 输出层：计算输出层残差 */
     v_double outputResidual(int label);
+
     /* 输出层：计算误差值 */
     double totalError(int label);
 
@@ -84,19 +91,26 @@ class BPNet {
 public:
     /* 初始化输入层，输出层 */
     BPNet(int dim, int num_classes, double learning_rate = 0.5);
+
     /* 增加隐藏层 */
     void addHiddenLayer(int node_num);
+
     /* 读取数据 数据格式： 二维数组 vector<v_double > 数据 标签 */
     void dataReader(const vector<v_double > &train_data,
                     const vector<v_double > &test_data);
+
     /* 训练 */
     double train(int epoch = 1);
+
     /* 预测 */
     double evaluate();
+
     /* 打印当前网络结构 */
     void summary();
+
     /* 模型保存 */
     void save(const string &path);
+
     /* 模型加载 */
     void load(const string &path);
 
@@ -118,8 +132,10 @@ private:
 
     /* 前向传播 */
     void forward(v_double value);
+
     /* 反向传播 */
     double backward(int label);
+
     /* 寻找最大值下标 */
     int findMax(v_double x);
 };
