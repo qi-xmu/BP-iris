1326ca300085a6fd817a50b1e04028b84494cea6
Qi <56908202+qi-xmu@users.noreply.github.com>
2021-05-25 09:59:02 +0800

    增加模型

--- main.cpp
+++ main.cpp
@@ -13,27 +13,25 @@ int main() {
     d.confuse(1000);    /* 数据混淆：打乱顺序 */
 
     vector<v_double> data = d.Data();                  /* 全部数据集 */
-    BPNet net(4, 3, 1);     /* 指定数据维度，分类数，学习率 */
+//    BPNet net(4, 3, 1);     /* 指定数据维度，分类数，学习率 */
+//
+//    net.dataReader(d.train_data, d.eval_data);         /* 加载数据集 */
+//
+//    /* 网络结构 */
+//    net.addHiddenLayer(4);
+////    net.addHiddenLayer(3);
+//    net.summary();                /* 打印网络结构 */
+//    clock_t start = clock();    /* 开始计时 */
+//    net.train(500);             /* 开始训练 */
+//    clock_t end = clock();      /* 结束计时 */
+//    cout << "训练时间：" << (end - start) / (double) CLOCKS_PER_SEC << endl;
+//    net.save("../best-3.0.model");
 
-    net.dataReader(d.train_data, d.eval_data);         /* 加载数据集 */
-
-    /* 网络结构 */
-    net.addHiddenLayer(4);
-//    net.addHiddenLayer(3);
-    net.summary();                /* 打印网络结构 */
-    clock_t start = clock();    /* 开始计时 */
-    net.train(200);             /* 开始训练 */
-    clock_t end = clock();      /* 结束计时 */
-    cout << "训练时间：" << (end - start) / (double) CLOCKS_PER_SEC << endl;
-    net.save("../best-2.0.model");
-
-    net.evaluate();               /* 开始评估 */
+//    net.evaluate();               /* 开始评估 */
 
     BPNet load_net(4,3,1);
-
     load_net.dataReader(d.train_data, data);
-    load_net.load("../best-1.model");
-
+    load_net.load("../best-2.0.model");
     load_net.evaluate();
     return 0;
 }
